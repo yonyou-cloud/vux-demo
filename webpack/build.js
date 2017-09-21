49e207328c91dd616b5a2009bc0ee44e4754a267
薛定谔的猫 <hh_2013@foxmail.com>
2017-09-21 10:29:04 +0800

    Chore: upgrade deps. (#2)
    * Chore: upgrade deps.
    
    * Fix: No PostCSS Config found.(fixes #1)
    
    * Chore: uglifyjs => babel-minify

--- webpack/build.js
+++ webpack/build.js
@@ -1,15 +1,9 @@
-var webpack = require('webpack')
+// var webpack = require('webpack')
 var merge = require('webpack-merge')
 var config = require('./base.js')
+var BabelWebpackPlugin = require('babel-minify-webpack-plugin')
 
-config.plugins.push(new webpack.optimize.UglifyJsPlugin({
-  compress: {
-    warnings: false
-  },
-  output: {
-    comments: false
-  }
-}))
+config.plugins.push(new BabelWebpackPlugin())
 
 module.exports = merge(config, {
   devtool: false
