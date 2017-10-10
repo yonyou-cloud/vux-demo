a56766fadae414ccf5dffe627618dd622ebfbe2d
翟延彬 <zhaiyba@yonyou.com>
2017-10-10 10:44:43 +0800

    修改开发环境配置文件

--- webpack/dev.js
+++ webpack/dev.js
@@ -21,7 +21,7 @@ new WebpackDevServer(compiler, {
   },
   // hot: true,
   compress: true, // gzip压缩
-  publicPath: 'http://localhost:8888/',
+  publicPath: 'http://localhost:9999/',
   contentBase: '../dist/', // 默认情况下，webpack-dev-server会从项目的根目录提供文件，可以通过此选项设置文件的目录名
   historyApiFallback: true // 当设置为true时，访问所有服务器上不存在的文件，都会被重定向到/，也就是index.html文件
-}).listen(8888)
+}).listen(9999)
